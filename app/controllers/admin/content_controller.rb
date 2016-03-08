d8c230a1772a92a8abcc26c17c823ddc305e26c2
jasminelee1 <jasminelee@berkeley.edu>
2016-03-08 08:17:45 +0000

    only allow admin

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -140,6 +140,9 @@ class Admin::ContentController < Admin::BaseController
   def real_action_for(action); { 'add' => :<<, 'remove' => :delete}[action]; end
 
   def new_or_edit
+    if !current_user.admin?
+      redirect_to root_url
+    end
     id = params[:id]
     id = params[:article][:id] if params[:article] && params[:article][:id]
     @article = Article.get_or_build_article(id)
